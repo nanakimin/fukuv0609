6ce6d70e9b03d5e8ab8a0ae62a1c6e83f8390773
A601-25 <oudontabetai0101@gmail.com>
2022-06-09 13:40:25 +0900

    復習0609

--- fukuv0609/Form1.cs
+++ fukuv0609/Form1.cs
@@ -17,5 +17,20 @@ namespace fukuv0609
             InitializeComponent();
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += int.Parse(textBox1.Text);
+            label1.Top += int.Parse(textBox2.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = $" + { int.Parse(textBox1.Text) + int.Parse(textBox2.Text)}";
+        }
     }
 }
